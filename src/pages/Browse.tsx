5cfaf772c86d86f01abb5ba248793def048048d4
bramastyapp <bramastyapp@gmail.com>
2024-09-29 11:45:42 +0700

    feat: integration api search recipe

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -1,7 +1,22 @@
 import CategoryWrapper from "../wrappers/CategoryWrapper";
 import BrowseFeaturedRecipesWrapper from "../wrappers/BrowseFeaturedRecipesWrapper";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Browse() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    if (searchQuery.length > 0) {
+      navigate(`/search?query=${searchQuery}`);
+    }
+  };
+
   return (
     <>
       <nav className="flex items-center justify-between px-5 mt-[30px]">
@@ -19,10 +34,7 @@ export default function Browse() {
         </a>
       </nav>
       <div id="SearchForm" className="px-5 mt-[30px]">
-        <form
-          action="search.html"
-          className="flex items-center rounded-full p-[5px_14px] pr-[5px] gap-[10px] bg-white shadow-[0_12px_30px_0_#D6D6D652] transition-all duration-300 focus-within:ring-1 focus-within:ring-[#FF4C1C]"
-        >
+        <div className="flex items-center rounded-full p-[5px_14px] pr-[5px] gap-[10px] bg-white shadow-[0_12px_30px_0_#D6D6D652] transition-all duration-300 focus-within:ring-1 focus-within:ring-[#FF4C1C]">
           <img
             src="/assets/images/icons/note-favorite.svg"
             className="w-6 h-6"
@@ -31,14 +43,19 @@ export default function Browse() {
           <input
             type="text"
             name="search"
+            value={searchQuery}
+            onChange={handleInputChange}
             id="search"
             className="appearance-none outline-none w-full font-semibold placeholder:font-normal placeholder:text-black"
             placeholder="Find our best food recipes"
           />
-          <button type="submit" className=" flex shrink-0 w-[42px] h-[42px]">
+          <button
+            onClick={handleSearch}
+            className=" flex shrink-0 w-[42px] h-[42px]"
+          >
             <img src="/assets/images/icons/search.svg" alt="icon" />
           </button>
-        </form>
+        </div>
       </div>
       <CategoryWrapper />
       <BrowseFeaturedRecipesWrapper />
